0b2436eaa6894aa54c0d153dcf359bd4fcfcdcb5
Lei Yao <yaoleiyaolei@gmail.com>
2017-08-03 02:09:36 -0700

    setup template structure

--- web/accounts/views.py
+++ web/accounts/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 # Create your views here.
 def login(request):
-    return HttpResponse("Hello, world. You're at the accounts login.")
+	return render(request, 'base.html', {})
