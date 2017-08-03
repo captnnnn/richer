48aa480b817d580f3439d5c2fe99c01f54312655
Lei Yao <yaoleiyaolei@gmail.com>
2017-08-03 01:45:45 -0700

    user model

--- web/accounts/models.py
+++ web/accounts/models.py
@@ -1,3 +1,9 @@
-from django.db import models
-
+from django_mongoengine import Document
+from django_mongoengine import fields
+from django_mongoengine.mongo_auth.models import User
 # Create your models here.
+
+class UserProfile(Document):
+	firstName = fields.StringField(max_length=64)
+	lastName = fields.StringField(max_length=64)
+	user = fields.ReferenceField(User)
