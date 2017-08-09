6a1218663e15236556cc82b0f2fc4eaafbe5101f
Lei Yao <yaoleiyaolei@gmail.com>
2017-08-09 01:05:37 -0700

    put cliet react onto dashboard django template

--- web/client-app/app/index.js
+++ web/client-app/app/index.js
@@ -1,3 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const mountNode = window.document.getElementById('appRoot');
 class HelloMessage extends React.Component {
   render() {
     return <div>Hello {this.props.name}</div>;
