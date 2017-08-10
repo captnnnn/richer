4a138bb83f6e5cbcce659af7f7554daca9baf3d9
Lei Yao <yaoleiyaolei@gmail.com>
2017-08-10 02:32:45 -0700

    load bootstrap using cdn instead

--- web/client-app/app/index.js
+++ web/client-app/app/index.js
@@ -2,7 +2,6 @@ const mountNode = window.document.getElementById('appRoot');
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-//import Bootstrap from 'bootstrap/dist/css/bootstrap.css';
 import './main.css';
 
 import {
