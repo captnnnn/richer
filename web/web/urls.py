0b7b227036a046178e78e9ce7ae40c09c7de3265
Lei Yao <yaoleiyaolei@gmail.com>
2017-08-10 03:05:32 -0700

    delete admin urls

--- web/web/urls.py
+++ web/web/urls.py
@@ -2,8 +2,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-
-	url(r'^admin/', include(admin.site.urls)),
 	url(r'^accounts/', include('accounts.urls')),
 
 	# we don't adding $ on purpose, so the react route still works when user refersh the page
