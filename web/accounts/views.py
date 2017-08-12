d187bee55da49caf77b10fbc14c4d019d5fa16b3
Lei Yao <yaoleiyaolei@gmail.com>
2017-08-11 19:04:32 -0700

    render form and added csrf token

--- web/accounts/views.py
+++ web/accounts/views.py
@@ -1,12 +1,23 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .forms import UserRegistrationForm
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 def login(request):
 	return render(request, 'base_anonymous.html', {})
 
 def register(request):
-	return render(request, 'accounts/register.html', {})
+	if request.method == 'POST':
+		form = UserRegistrationForm(request.POST)
+		if form.is_valid():
+			return HttpResponseRedirect('/accounts/login/')
+	else:
+		form = UserRegistrationForm()
+
+	return render(request, 'accounts/register.html', {
+		'form': form,
+	})
 
 # Create your views here.
 def dashboard(request):
