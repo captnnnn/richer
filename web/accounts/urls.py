31158dc3738d4b9db0280b6936319692da77e63c
Lei Yao <yaoleiyaolei@gmail.com>
2017-08-12 02:08:03 -0700

    configured lgout

--- web/accounts/urls.py
+++ web/accounts/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
-
+from django.contrib.auth.views import logout_then_login
 from . import views
 
 urlpatterns = [
 	url(r'^login/$', views.login),
 	url(r'^register/$', views.register),
+	url(r'^logout/$', logout_then_login),
 ]
