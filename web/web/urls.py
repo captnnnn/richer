8904c56b0aabf51a20b23e8f64d8ce6f6803c016
Lei Yao <yaoleiyaolei@gmail.com>
2017-08-17 00:33:55 -0700

    graphql login required

--- web/web/urls.py
+++ web/web/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from graphene_django.views import GraphQLView
 from web.schema import schema
+from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
 	url(r'^accounts/', include('accounts.urls')),
@@ -9,5 +10,5 @@ urlpatterns = [
 	# we don't adding $ on purpose, so the react route still works when user refersh the page
 	url(r'^dashboard/', 'accounts.views.dashboard'),
 	url(r'^ajaxTest/', 'accounts.views.ajaxTest'),
-	url(r'^graphql', GraphQLView.as_view(graphiql=True, schema=schema)),
+	url(r'^graphql', login_required(GraphQLView.as_view(graphiql=True, schema=schema))),
 ]
