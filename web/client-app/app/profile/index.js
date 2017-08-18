0e749d4d5f95d5213c4d2e86acd8139275fe7aba
Lei Yao <yaoleiyaolei@gmail.com>
2017-08-18 01:50:44 -0700

    made toggling work

--- web/client-app/app/profile/index.js
+++ web/client-app/app/profile/index.js
@@ -5,37 +5,55 @@ import {
 } from 'react-apollo';
 
 class UserProfile extends React.Component {
-	handleClick(e) {
-		console.log('this is:' + e);
+	constructor() {
+		super();
+		this.state = {
+			isEditing: false,
+		};
+	}
+
+	handleClick(action) {
+		if (this.state.isEditing) {
+			this.setState({isEditing: false});
+		}
+		else {
+			this.setState({isEditing: true});
+		}
 	}
 
 	render() {
-		let messageToShow = (<p> No Data.</p>);
 		if (this.props.data.loading) {
-			messageToShow = (<p> Loading </p>);
+			return <p> Loading </p>;
 		}
 		if (this.props.data.error) {
-			messageToShow = (<p> error: {this.props.data.error.message} </p>);
+			return <p> error: {this.props.data.error.message} </p>;
 		}
-		if (this.props.data.myProfile) {
-			const profile = this.props.data.myProfile;
-			messageToShow = (
+
+		const profile = this.props.data.myProfile;
+		const header = <h2> Profile </h2>;
+		if (this.state.isEditing) {
+			return (
 				<div>
-					<p> first Name: {profile.firstName} </p>
-					<p> last Name: {profile.lastName} </p>
+					{header}
+					 <button onClick={(e) => this.handleClick(e)}>
+						Save
+					</button>
+					<p> Edit first Name: {profile.firstName} </p>
+					<p> Edit last Name: {profile.lastName} </p>
 				</div>
-			)
+			);
 		}
 		return (
 			<div>
-				<h2> Profile </h2>
-				{messageToShow}
+				{header}
 				 <button onClick={(e) => this.handleClick(e)}>
-					Click me
+					Edit
 				</button>
+				<p> first Name: {profile.firstName} </p>
+				<p> last Name: {profile.lastName} </p>
 			</div>
+		);
 
-		)
 	}
 };
 
